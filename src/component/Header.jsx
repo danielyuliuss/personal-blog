c79c8621211f3a8903898a51a9ee7b86e8d7c929
danielyuliuss <danielyuliussoetjipto@gmail.com>
2023-11-04 11:08:26 +0700

    1

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
  const Header = () => {
-    return <div>Header</div>
+    return <div>
+        <div><img src="https://media.licdn.com/dms/image/D5603AQHI8uq8fvaqeg/profile-displayphoto-shrink_800_800/0/1676882495046?e=1704326400&v=beta&t=-yLlif0jHKTynP0ViN0UvJDoh8M6FXb31MR5Sji4rQk" alt="" /></div>
+    </div>
  }
 
  export default Header;
