b44d95938970bb9c816b493e151352677912875a
danielyuliuss <danielyuliussoetjipto@gmail.com>
2023-11-04 11:11:16 +0700

    2

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
  const Header = () => {
     return <div>
-        <div><img src="https://media.licdn.com/dms/image/D5603AQHI8uq8fvaqeg/profile-displayphoto-shrink_800_800/0/1676882495046?e=1704326400&v=beta&t=-yLlif0jHKTynP0ViN0UvJDoh8M6FXb31MR5Sji4rQk" alt="" /></div>
+        <div className="w-[100px] h-[100px] rounded-full overflow-hidden">
+            <img className="object-cover object-center w-full" src="https://media.licdn.com/dms/image/D5603AQHI8uq8fvaqeg/profile-displayphoto-shrink_800_800/0/1676882495046?e=1704326400&v=beta&t=-yLlif0jHKTynP0ViN0UvJDoh8M6FXb31MR5Sji4rQk" alt="" />
+            </div>
     </div>
  }
 
